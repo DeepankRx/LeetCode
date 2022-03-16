137e7974d374ba33b35f39abf2767c4e85f19a18
DeepankRx <49677833+DeepankRx@users.noreply.github.com>
2022-03-16 17:26:11 +0530

    Time: 14 ms (5.79%), Space: 17.4 MB (6.24%) - LeetHub

--- 872-leaf-similar-trees/872-leaf-similar-trees.cpp
+++ 872-leaf-similar-trees/872-leaf-similar-trees.cpp
@@ -30,13 +30,6 @@ public:
        vector<int> ans1,ans2;
         ans1 = inOrder(root1,ans1);
         ans2 = inOrder(root2,ans2);
-        if(ans1.size() != ans2.size())
-            return false;
-        for(int i=0;i<ans1.size();i++)
-        {
-            if(ans1.at(i)!=ans2.at(i))
-                return false;
-        }
-        return true;
+        return ans1==ans2;
     }
 };
