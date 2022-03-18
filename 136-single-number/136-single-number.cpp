0cc67fef417dfe880ded7bf4a528a766105c3a2e
DeepankRx <49677833+DeepankRx@users.noreply.github.com>
2022-03-18 12:24:50 +0530

    Time: 29 ms (46.08%), Space: 16.8 MB (85.52%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i+=2)
+        int ans = 0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i]!=nums[i-1])
-                return nums[i-1];
+            ans = ans^ nums.at(i); 
         }
-        return nums[nums.size()-1];
+        return ans;
     }
 };
