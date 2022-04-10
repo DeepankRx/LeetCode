ada5e3b18200a3194fbaa5455dbeec5d3fa45d9a
DeepankRx <49677833+DeepankRx@users.noreply.github.com>
2022-04-10 23:35:08 +0530

    Time: 8 ms (30.74%), Space: 8.4 MB (28.60%) - LeetHub

--- 682-baseball-game/682-baseball-game.cpp
+++ 682-baseball-game/682-baseball-game.cpp
@@ -34,39 +34,3 @@ public:
         return sum;
     }
 };
-// class Solution {
-// public:
-//     int calPoints(vector<string>& ops) {
-//         vector<int> ans ;
-//         int size =ops.size() ;
-        
-//         for(int i= 0 ; i<ops.size(); i++){\
-            
-//             if(ops[i]=="+"){
-//                 int prevSum=0 ;
-//                 prevSum=ops[size-2]+ops[size-1] ;
-//                 ans.push_back(prevSum) ;
-//             }
-            
-//             if(ops[i]=="D"){
-//                 int double=(ops[size-1])*2;
-//                 ans.push_back(double) ;
-//             }
-            
-//             if(ops[i]=="C"){
-//                 ans.pop_back() ;
-//             }
-                                          
-//             else{
-//                 ans.push_back(stoi(ops[i])) ;
-//             }
-//         }
-        
-//         int sum=0 ;
-//         for(int i=0 ; i<ans.size(); i++)
-//             sum+=ans[i] ;
-        
-        
-//         return sum;
-//     }
-// };
