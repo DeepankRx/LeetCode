1c77392d6f86f5527a5879eeb87fcbcb71c7d385
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-05-16 16:18:01 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (10.29%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,4 +1,5 @@
-//memoziation
+//memoziation (top to bottom)
+/*
 class Solution {
     int fib(int n,vector<int> &dp)
     {
@@ -11,5 +12,31 @@ public:
     int fib(int n) {
       vector<int> dp(n+1,-1);
         return fib(n,dp);
+    }
+};
+*/
+
+//tabulation
+class Solution {
+public:
+    int func(int n,vector<int> &dp)
+    {
+        if(n==0 || n==1)
+            return n;
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for(int i=2;i<=n;i++)
+            dp[i] = dp[i-1] + dp[i-2];
+        
+        return dp[n];
+    }
+    
+    
+    int fib(int n) {
+        vector<int> dp(n+1,-1);
+        
+        return func(n,dp);                
     }
 };
