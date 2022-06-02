1acf38cad6e17290c442c86e79ebd4a45ddde03f
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-06-02 11:48:21 +0530

    Time: 34 ms (5.17%), Space: 10.7 MB (13.79%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-      vector<vector<int>> transposeMatrix(matrix[0].size(), vector<int>(matrix.size(), 0));
-	for (int i = 0; i < matrix.size(); i++) {
-		for (int j = 0; j < matrix[0].size(); j++) {
-			transposeMatrix[j][i] = matrix[i][j];
-		}
-	}
-	return transposeMatrix;
+ vector<vector<int>> t(matrix[0].size(), vector<int>(matrix.size(), 0));
+        for(int i = 0 ; i < matrix.size();i++)
+        {
+            for(int j =0 ; j <matrix[i].size();j++)
+            {
+               t[j][i]=matrix[i][j];
+            }
+        }
+        return t;
     }
 };
