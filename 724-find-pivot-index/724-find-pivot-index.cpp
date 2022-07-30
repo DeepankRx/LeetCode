2d8b7cafa39cb655ac4b86c36f2463e8fdeb56d7
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-07-30 13:45:21 +0530

    Time: 26 ms (89.32%), Space: 31.2 MB (53.50%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int rightSum = accumulate(nums.begin(),nums.end(),0);
-        int leftSum = 0;
-        for(int i=0;i<nums.size();i++){            
-            rightSum -= nums[i];
-            if(leftSum == rightSum){
+        int rightSum = accumulate(nums.begin(),nums.end(), 0);
+        int leftSum = 0 ;
+        for(int i = 0 ; i <nums.size()  ; i++)
+        {
+            rightSum -= nums.at(i);
+            if(rightSum == leftSum)
+            {
                 return i;
             }
-            leftSum += nums[i];
+            leftSum += nums.at(i);
         }
         return -1;
     }
