a7a0d68853c3c158177087229e05263dd8c1d99e
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-08-31 23:47:13 +0530

    Time: 95 ms (86.92%), Space: 42.1 MB (9.03%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin() , people.end()) ;
-        int low=0;
-        int high=people.size()-1 ;
-        
-        int count=0;
-        
-        while(low<=high){
-            int sum=people[low] + people[high] ;
-            
-            if(sum <= limit){
-                count++ ;
-                high-- ;
-                low++ ;
+        sort(people.begin(),people.end());
+        int start = 0 ,end = people.size()-1,count=0;
+        while(start<=end)
+        {
+            int sum =   people.at(start)+people.at(end);
+            if(sum<=limit)
+            {
+                count++;
+                start++;
+                end--;
             }
-            else if(sum > limit){
-                count++ ;
-                high-- ;
+            else if(sum>limit)
+            {
+                end--;   
+                count++;
             }
         }
-        return count ;
+        return count;
     }
 };
