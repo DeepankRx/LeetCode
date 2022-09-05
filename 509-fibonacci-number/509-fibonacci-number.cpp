a4e2cf827dec85dda55192e1f685b7ddbfa89e84
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-09-05 21:36:12 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.63%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,64 +1,17 @@
-//memoziation (top to bottom)
-/*
-class Solution {
-    int fib(int n,vector<int> &dp)
+class Solution
+{
+    int fib(int n, vector<int>&dp)
     {
-          if(n<=1)
+        if (n <= 1)
             return n;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = fib(n-1,dp) + fib(n-2,dp);
+        if (dp[n] != -1)
+            return dp[n];
+        return dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
     }
-public:
-    int fib(int n) {
-      vector<int> dp(n+1,-1);
-        return fib(n,dp);
-    }
-};
-*/
-
-//tabulation
-// class Solution {
-// public:
-//     int func(int n,vector<int> &dp)
-//     {
-//         if(n==0 || n==1)
-//             return n;
-        
-//         dp[0] = 0;
-//         dp[1] = 1;
-        
-//         for(int i=2;i<=n;i++)
-//             dp[i] = dp[i-1] + dp[i-2];
-        
-//         return dp[n];
-//     }
-    
-    
-//     int fib(int n) {
-//         vector<int> dp(n+1,-1);
-        
-//         return func(n,dp);                
-//     }
-// };
-
-
-//space optimisation
-class Solution {
-public:
-    int fib(int n) {
-        int prev2 = 0;
-        int prev = 1;
-        
-        if(n==0 || n==1)
-            return n;
-        
-        for(int i=2;i<=n;i++)
+    public:
+        int fib(int n)
         {
-            int curr = prev2 + prev;
-            prev2 = prev;
-            prev = curr;
+            vector<int> dp(n + 1, -1);
+            return fib(n, dp);
         }
-        
-        return prev;
-    }
 };
