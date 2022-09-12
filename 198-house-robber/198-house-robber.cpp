55499a23af3e71c89e2a499e8a40848297f5cbc3
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-09-12 22:48:39 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (49.54%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,21 @@
-class Solution {
-    int func(int index,vector<int> &nums,vector<int> &dp)
+class Solution
+{
+    int func(int ind, vector<int> &nums, vector<int> &dp)
+
     {
-        if(index==0)
-            return nums[index];
-        if(index<0) 
-            return 0;
-        if(dp[index]!=-1)
-            return dp[index];
-        int pick = nums[index] + func(index - 2,nums,dp);
-        int notPick = 0 + func(index-1,nums,dp);
-        return dp[index] = max(pick,notPick);
+        if (ind == 0) return nums[ind];
+        if (ind < 0) return 0;
+        if (dp[ind] != -1)
+            return dp[ind];
+        int pick = nums[ind] + func(ind - 2, nums, dp);
+        int notPick = 0 + func(ind - 1, nums, dp);
+        return dp[ind] = max(pick, notPick);
     }
-public:
-    int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,-1);
-        return func(n-1,nums,dp);
-    }
+    public:
+        int rob(vector<int> &nums)
+        {
+            int n = nums.size();
+            vector<int> dp(n + 1, -1);
+            return func(n - 1, nums, dp);
+        }
 };
