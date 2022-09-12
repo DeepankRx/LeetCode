079333a9f5961c511dd6924573f234a964099567
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-09-12 22:58:45 +0530

    Time: 8 ms (7.59%), Space: 7.9 MB (25.92%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,22 @@
-class Solution
-{
-    int func(int ind, vector<int> &nums, vector<int> &dp)
-
-    {
-        if (ind == 0) return nums[ind];
-        if (ind < 0) return 0;
-        if (dp[ind] != -1)
-            return dp[ind];
-        int pick = nums[ind] + func(ind - 2, nums, dp);
-        int notPick = 0 + func(ind - 1, nums, dp);
-        return dp[ind] = max(pick, notPick);
-    }
-    public:
-        int rob(vector<int> &nums)
-        {
-            int n = nums.size();
-            vector<int> dp(n + 1, -1);
-            return func(n - 1, nums, dp);
+class Solution {
+public:
+    // Tabulation Method
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        
+        // if nums has only one element then that element will be output
+        if(n==1)    return nums[0];
+        
+        vector<int> dp(n, -1);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0], nums[1]);
+        
+        for(int i=2; i<n; i++){
+            int take = nums[i]+dp[i-2];        //if we take index i then we can't move to index i-1
+            int notTake = 0 + dp[i-1];         //here we didn't take index i so we can move to index i-1
+            dp[i] = max(take, notTake);
         }
+        
+        return dp[n-1];
+    }
 };
