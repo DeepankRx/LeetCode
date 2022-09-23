bbd752cd4fbeec1049699425f0e83a974105582c
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-09-23 10:57:33 +0530

    Time: 143 ms (62.13%), Space: 54.9 MB (6.71%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,10 +1,18 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size()-1; i++) 
-            if(nums[i] == nums[i+1]) 
-                return true;
-        return false;
-    }
+class Solution
+{
+    public:
+        bool containsDuplicate(vector<int> &nums)
+        {
+            map<int, int> count;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                count[nums[i]]++;
+            }
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (count[nums[i]] > 1)
+                    return true;
+            }
+            return false;
+        }
 };
