e7f9205bac65d645eb5b36f2c28e76087c64199f
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-09-26 16:17:11 +0530

    Time: 27 ms (56.24%), Space: 10.8 MB (52.10%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,30 +1,21 @@
-class Solution {
-    vector<int> twoSum(vector<int> nums,int start,int end,int target)
-    {
-        if(start>=end)
+class Solution
+{
+    public:
+        vector<int> twoSum(vector<int> &nums, int target)
         {
-            return nums;
+            unordered_map<int, int> mp;
+            for (int i = 0; i < nums.size(); i++)
+            {
+                if (mp.find(target - nums.at(i)) != mp.end())
+                {
+                    return {
+                        i,
+                        mp[target - nums.at(i)]
+                    };
+                }
+                mp[nums[i]] = i;
+            }
+            return { -1,
+                -1 };
         }
-            if(nums.at(start)+nums.at(end)==target)
-                return {start,end};
-            else if(nums.at(start)+nums.at(end)<target)
-                return twoSum(nums,start+1,end,target);
-            else
-                return twoSum(nums,start,end-1,target);
-      
-    }
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> temp(nums),result;
-        sort(nums.begin(),nums.end());
-        vector<int> points = twoSum(nums,0,nums.size()-1,target);
-        cout<<points.at(0)<<" "<<points.at(1)<<endl;
-        for(int i=0;i<temp.size();i++)
-        {
-            cout<<temp.at(i)<<" ";
-            if(temp[i]==nums.at(points.at(0)) ||temp[i]==nums.at(points.at(1)))
-                result.push_back(i);
-        }
-        return result;
-    }
 };
