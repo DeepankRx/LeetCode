5af32b4f9dc4c9d805249c06ae22b1254a53a668
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-10-08 15:15:41 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (31.68%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,29 +1,19 @@
-class Solution {
-    //memoziation
-    // int helper(int n , vector<int> dp)
-    // {
-    //     if(n<=1)
-    //         return 1;
-    //     if(dp[n]!=-1) return dp[n];
-    //     return dp[n] = helper(n-1,dp) + helper(n-2,dp);
-    // }
-    
-    //tabulation
-    int helper(int n,vector<int> dp)
+class Solution
+{
+    int f(int i, vector<int> &dp)
     {
-        if(n==0 || n==1)
-            return n;
-        dp[0]=1;
-        dp[1]=1;
-        for(int i = 2;i<=n;i++)
+        if (i == 0)
+            return 1;
+        if (i < 0)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        return dp[i] = f(i - 1, dp) + f(i - 2, dp);
+    }
+    public:
+        int climbStairs(int n)
         {
-            dp[i]=dp[i-1]+dp[i-2];
+            vector<int> dp(n + 1, -1);
+            return f(n, dp);
         }
-        return dp[n];
-    }
-public:
-    int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return helper(n,dp);
-    }
 };
