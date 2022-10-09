0bb24e30203867e3a2b381c259f0fef88a56ea00
Deepank Pushpad <49677833+DeepankRx@users.noreply.github.com>
2022-10-09 21:48:24 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (6.88%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,36 +1,33 @@
 class Solution
 {
+    int f(vector<int> &nums, int i, vector<int> &dp)
+    {
+        if (i < 0)
+            return 0;
+        if (i == 0)
+            return nums[i];
+        if (dp[i] != -1)
+            return dp[i];
+        int pick = nums[i] + f(nums, i - 2, dp);
+        int notPick = f(nums, i - 1, dp);
+        return dp[i] = max(pick, notPick);
+    }
     public:
         int rob(vector<int> &nums)
         {
-
             int n = nums.size();
-            if (n == 1) return nums[0];
-            if (n == 2) return max(nums[0], nums[1]);
-
-           	// dp1 -> 1 to n-1
-           	// dp2 -> 2 to n
-
-            int dp1[n], dp2[n];
-
-           	// if we have robbed the first house, then we can't rob the second house
-            dp1[0] = nums[0];
-            dp1[1] = nums[0];
-
-           	// if we didn't rob the first house, we will definetely rob the second
-            dp2[0] = 0;
-            dp2[1] = nums[1];
-
-            for (int i = 2; i < n; i++)
+                if(n==1)
+                        return nums.at(0);
+            vector<int> dp1(n, -1);
+            vector<int> dp2(n, -1);
+            vector<int> excludingFirst, excludingLast;
+            for (int i = 0; i < n; i++)
             {
-
-               	// make optimal choice at each house
-                dp1[i] = max(nums[i] + dp1[i - 2], dp1[i - 1]);
-                dp2[i] = max(nums[i] + dp2[i - 2], dp2[i - 1]);
+                if (i != 0)
+                    excludingFirst.push_back(nums.at(i));
+                if (i != n - 1)
+                    excludingLast.push_back(nums.at(i));
             }
-
-           	// dp1 is for nums[1 - (n-1)] so we won't consider the last house in this case and dp1[n-2] will be max profit
-           	// and dp2 is for nums[2 - n] so we can take the last house
-            return max(dp1[n - 2], dp2[n - 1]);
+            return max(f(excludingFirst, n - 2, dp1), f(excludingLast, n - 2, dp2));
         }
 };
